62eff64bfd9318a0d98b81f113dad426c5c0a0a7
Hao1998 <hienhao142@gmail.com>
2019-08-21 15:21:22 +0700

    modify URL

--- webvitinh/Startup.cs
+++ webvitinh/Startup.cs
@@ -191,6 +191,10 @@ namespace webvitinh
                   template: "{controller=Product}/{action=Search}/{id?}");
 
                 routes.MapRoute(
+                  name: "Product Index",
+                  template: "{controller=Product}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                   name: "default",
                   template: "{controller=Home}/{action=Index}/{id?}");
             });
